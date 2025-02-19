3a2e34af753d1651aa5728d1badf6bc90de11d9f
Yuvaraj Vasam <110529330+yuvarajvasam@users.noreply.github.com>
2025-02-20 00:52:30 +0530

    Update next.config.mjs

--- frontend/src/next.config.mjs
+++ frontend/src/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,  
+  },
+  output: "standalone",
+};
 
 export default nextConfig;
